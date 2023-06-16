a44c72f3a80858c21bc74b696130e04524853a94
illiyyin <iilliyyin@gmail.com>
2023-06-16 20:11:05 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import SideMenu from './components/SideMenu'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <div style={{ display: 'flex' }}>
+        <SideMenu /> {/* Ditambahkan */}
+        <h1>Home</h1>
+      </div>
+    ),
+  },
+  {
+    path: 'task-list',
+    element: (
+      <div style={{ display: 'flex' }}>
+        <SideMenu /> {/* Ditambahkan */}
+        <h1>Task List</h1>
+      </div>
+    ),
+  },
+  {
+    path: 'task-progress',
+    element: (
+      <div style={{ display: 'flex' }}>
+        <SideMenu /> {/* Ditambahkan */}
+        <h1>Task Progress</h1>
+      </div>
+    ),
+  },
+])
+
+function App(): JSX.Element {
+  return <RouterProvider router={router} />
+}
+
+export default App
---
+++ src/types/index.ts
@@ -0,0 +1,4 @@
+import React from 'react'
+export interface CSSProperties {
+  [key: string]: React.CSSProperties
+}
